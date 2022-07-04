7640973f10bc874d4b00dcc4abedd062889fd35f
Jesse Williamson <jessewilliamson@fastmail.fm>
2022-07-04 13:18:12 -0700

    move_semantics

---
+++ move_semantics/move_semantics3.rs
@@ -0,0 +1,46 @@
+// move_semantics3.rs
+// Make me compile without adding new lines-- just changing existing lines!
+// (no lines with multiple semicolons necessary!)
+// Execute `rustlings hint move_semantics3` for hints :)
+
+// Original
+// fn main() {
+//     let vec0 = Vec::new();
+
+//     let mut vec1 = fill_vec(vec0);
+
+//     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+
+//     vec1.push(88);
+
+//     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+// }
+
+// fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+//     vec.push(22);
+//     vec.push(44);
+//     vec.push(66);
+
+//     vec
+// }
+
+// Updated
+fn main() {
+    let mut vec0 = Vec::new();
+
+    let mut vec1 = fill_vec(vec0);
+
+    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+
+    vec1.push(88);
+
+    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+}
+
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+
+    vec
+}
