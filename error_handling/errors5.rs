30a7ece7651dcaf6ca34fd4150cc56ba177b1244
Jesse Williamson <jessewilliamson@fastmail.fm>
2022-11-05 11:20:41 -0700

    error_handling

---
+++ error_handling/errors5.rs
@@ -0,0 +1,103 @@
+// errors5.rs
+
+// This program uses a completed version of the code from errors4.
+// It won't compile right now! Why?
+// Execute `rustlings hint errors5` for hints!
+
+// ORGINAL
+// use std::error;
+// use std::fmt;
+// use std::num::ParseIntError;
+
+// // TODO: update the return type of `main()` to make this compile.
+// fn main() -> Result<(), ParseIntError> {
+//     let pretend_user_input = "42";
+//     let x: i64 = pretend_user_input.parse()?;
+//     println!("output={:?}", PositiveNonzeroInteger::new(x)?);
+//     Ok(())
+// }
+
+// // Don't change anything below this line.
+
+// #[derive(PartialEq, Debug)]
+// struct PositiveNonzeroInteger(u64);
+
+// #[derive(PartialEq, Debug)]
+// enum CreationError {
+//     Negative,
+//     Zero,
+// }
+
+// impl PositiveNonzeroInteger {
+//     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
+//         match value {
+//             x if x < 0 => Err(CreationError::Negative),
+//             x if x == 0 => Err(CreationError::Zero),
+//             x => Ok(PositiveNonzeroInteger(x as u64))
+//         }
+//     }
+// }
+
+// // This is required so that `CreationError` can implement `error::Error`.
+// impl fmt::Display for CreationError {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         let description = match *self {
+//             CreationError::Negative => "number is negative",
+//             CreationError::Zero => "number is zero",
+//         };
+//         f.write_str(description)
+//     }
+// }
+
+// impl error::Error for CreationError {}
+
+// UPDATED
+use std::error;
+use std::fmt;
+use std::num::ParseIntError;
+
+// .parse can return different types of errors
+// change the return type to anything implments the error trait
+// that's a lot of things and so the compiler can't figure how
+// space to allocate on the stack
+// wrapping it Box<T> so Rust stores it on the heap.
+fn main() -> Result<(), Box<error::Error>>{
+    let pretend_user_input = "42";
+    let x: i64 = pretend_user_input.parse()?;
+    println!("output={:?}", PositiveNonzeroInteger::new(x)?);
+    Ok(())
+}
+
+// Don't change anything below this line.
+
+#[derive(PartialEq, Debug)]
+struct PositiveNonzeroInteger(u64);
+
+#[derive(PartialEq, Debug)]
+enum CreationError {
+    Negative,
+    Zero,
+}
+
+impl PositiveNonzeroInteger {
+    fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
+        match value {
+            x if x < 0 => Err(CreationError::Negative),
+            x if x == 0 => Err(CreationError::Zero),
+            x => Ok(PositiveNonzeroInteger(x as u64))
+        }
+    }
+}
+
+// This is required so that `CreationError` can implement `error::Error`.
+impl fmt::Display for CreationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let description = match *self {
+            CreationError::Negative => "number is negative",
+            CreationError::Zero => "number is zero",
+        };
+        f.write_str(description)
+    }
+}
+
+impl error::Error for CreationError {}
